fabbf69b800fb1c7ef1c9c345c71bfc49a482ceb
yakov152005 <155822770+yakov152005@users.noreply.github.com>
2024-04-05 17:09:07 +0300

    Update Worker.java

--- TestOOP2/src/Worker.java
+++ TestOOP2/src/Worker.java
@@ -32,19 +32,9 @@ public class Worker {
 	}
 
 	public boolean isCEO() {
-		if (directManager == null) {
-			for (Worker subordinate : subordinates) {
-				if (subordinate.directManager != this) {
-					return false; 
-				}
-				if (!subordinate.isCEO()) {
-					return false; 
-				}
-			}
-			return true;
-		}
-		return false; 
+		return directManager == null;
 	}
+
 	public void addSubordinate(Worker subordinate) {
 		Worker[] newSubordinates = new Worker[subordinates.length + 1];
 		for (int i = 0; i < subordinates.length; i++) {
@@ -55,4 +45,5 @@ public class Worker {
 	}
 
 }
+
 
