9bac9ee0080d3d34946cb2d003e270cffd748574
yakov152005 <155822770+yakov152005@users.noreply.github.com>
2024-04-05 16:15:05 +0300

    Update MainEx1.java

--- TestOOP2/src/MainEx1.java
+++ TestOOP2/src/MainEx1.java
@@ -1,23 +1,23 @@
 public class MainEx1 {
 	public static void main(String[] args) {
 		Worker[] worker = {
-				new Worker("yakov", 28,"Yakov"),
-				new Worker("shilo", 24,"Yakov"),
-				new Worker("daniel", 23,"Yakov"),
-				new Worker("ido", 22,"Yakov"),
-				new Worker("amit", 28,"Yakov"),
-				new Worker("itay", 21,"Yakov"),
-				new Worker("SHAKED", 32,"Yakov"),
-				new Worker("LIAT", 41,"Yakov")
+				new Worker("yakov", 28,null),
+				new Worker("shilo", 24,null),
+				new Worker("daniel", 23,null),
+				new Worker("ido", 22,null),
+				new Worker("amit", 28,null),
+				new Worker("itay", 21,null),
+				new Worker("SHAKED", 32,null),
+				new Worker("LIAT", 41,null)
 		};
-		worker[0].addWorker(worker[1]);
-		worker[1].addWorker(worker[2]);
-		worker[1].addWorker(worker[3]);
-		worker[1].addWorker(worker[4]);
-		worker[4].addWorker(worker[5]);
-		worker[4].addWorker(worker[6]);
-		worker[4].addWorker(worker[7]);
-		System.out.println(worker[0].countTotalManagedWorker());
+		worker[0].addSubordinate(worker[1]);
+		worker[1].addSubordinate(worker[2]);
+		worker[1].addSubordinate(worker[3]);
+		worker[1].addSubordinate(worker[4]);
+		worker[4].addSubordinate(worker[5]);
+		worker[4].addSubordinate(worker[6]);
+		worker[4].addSubordinate(worker[7]);
+		System.out.println(worker[0].countTotalManagedWorkers());
 		System.out.println(worker[0].isCEO());
 
 	}
